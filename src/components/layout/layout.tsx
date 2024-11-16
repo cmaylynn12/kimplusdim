d0c2bf8647ad0026542237349094e29238f633a3
Kimberley Chan <cmaylynn12@gmail.com>
2024-11-16 19:59:23 +0100

    Added info button

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,19 +1,31 @@
-import { useContext, useState } from 'react';
-import MenuItem from '../menuItem/menuItem';
-import './layout.css';
-import AppContext from '../../contexts/AppContext';
-import Dropdown from '../dropdown/dropdown';
+import { useContext, useState } from "react";
+import MenuItem from "../menuItem/menuItem";
+import "./layout.css";
+import AppContext from "../../contexts/AppContext";
+import Dropdown from "../dropdown/dropdown";
 
 interface LayoutProps {
   children?: React.ReactNode;
-  activeSection: "home" | "accommodation" | "wedding" | "rsvp" | "registry" | "activities" | "faqs" | "info";
+  activeSection:
+    | "home"
+    | "accommodation"
+    | "wedding"
+    | "rsvp"
+    | "registry"
+    | "activities"
+    | "faqs"
+    | "info";
   title?: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({children, activeSection, title}: LayoutProps) => {
-  
-  const { mute, unmute, isMuted, setIsMuted, isMobile } = useContext(AppContext);
-  const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  activeSection,
+  title,
+}: LayoutProps) => {
+  const { mute, unmute, isMuted, setIsMuted, isMobile } =
+    useContext(AppContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleSoundButtonToggle = () => {
     if (isMuted) {
@@ -21,33 +33,59 @@ const Layout: React.FC<LayoutProps> = ({children, activeSection, title}: LayoutP
     } else {
       mute();
     }
-    setIsMuted(!isMuted)
-  }
+    setIsMuted(!isMuted);
+  };
 
   return (
     <>
-      <div className='body'>
-        <div className='header'>
-        { isMobile && <Dropdown toggle={() => setIsMenuOpen(!isMenuOpen)} isOpen={isMenuOpen}/>}
-        <div className={`menu ${isMenuOpen || !isMobile ? 'show' : 'hide'}`}>
-            <MenuItem activeSection={activeSection} section='home' title='Home' urlPath='/'/>
-            <MenuItem activeSection={activeSection} section= 'rsvp' title='RSVP' urlPath='/rsvp'/>
-            {/* <MenuItem activeSection={activeSection} section= 'info' title='Information' urlPath='/information'/> */}
+      <div className="body">
+        <div className="header">
+          {isMobile && (
+            <Dropdown
+              toggle={() => setIsMenuOpen(!isMenuOpen)}
+              isOpen={isMenuOpen}
+            />
+          )}
+          <div className={`menu ${isMenuOpen || !isMobile ? "show" : "hide"}`}>
+            <MenuItem
+              activeSection={activeSection}
+              section="home"
+              title="Home"
+              urlPath="/"
+            />
+            <MenuItem
+              activeSection={activeSection}
+              section="rsvp"
+              title="RSVP"
+              urlPath="/rsvp"
+            />
+            <MenuItem
+              activeSection={activeSection}
+              section="info"
+              title="Information"
+              urlPath="/information"
+            />
             {/* <MenuItem activeSection={activeSection} section= 'accommodation' title='Accommodation' urlPath='/accommodation' />
             <MenuItem activeSection={activeSection} section= 'wedding' title='Wedding' urlPath='/wedding' />
             <MenuItem activeSection={activeSection} section= 'activities' title='Things to Do' urlPath='/activities' />
             <MenuItem activeSection={activeSection} section= 'registry' title='Registry' urlPath='/registry' />
             <MenuItem activeSection={activeSection} section= 'faqs' title='FAQs' urlPath='/faqs' /> */}
-          </div> 
-        </div> 
-        <img className="volume" src={isMuted ? "/mute.svg" : "/unmute.svg"} width={30} height={30} onClick={handleSoundButtonToggle} />
-        <div className='main'>
-          { title && <p className="title">{title}</p>}
+          </div>
+        </div>
+        <img
+          className="volume"
+          src={isMuted ? "/mute.svg" : "/unmute.svg"}
+          width={30}
+          height={30}
+          onClick={handleSoundButtonToggle}
+        />
+        <div className="main">
+          {title && <p className="title">{title}</p>}
           {children}
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Layout;
