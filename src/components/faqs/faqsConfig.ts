671dbb5df765da2cc5fae47f37ed772b624e802e
Kimberley Chan <cmaylynn12@gmail.com>
2024-12-18 22:50:25 +0100

    Adjusted faqs

--- src/components/faqs/faqsConfig.ts
+++ src/components/faqs/faqsConfig.ts
@@ -4,43 +4,43 @@ export interface FAQ {
 }
 export const FAQs: FAQ[] = [
   {
-    question: "When and where is the wedding?",
+    question: "1/ When and where is the wedding?",
     answer:
       "The location will be at the Island Art & Taste - Gallery venue. The ceremony will begin at 6PM, following that we'll have a cocktail hour which includes a traditional tea creremony, then the reception where there will be plenty of food and drinks to continue the celebration.",
   },
   {
-    question: "What should I wear?",
+    question: "2/ What should I wear?",
     answer:
       "The dress code is cocktail attire. Think a suit or blazer combo for the gents and something cute and colorful for the ladies. As the evening may get cooler, consider bringing a light jacket or wrap. We definitely recommend comfrotable shoes as there will be plenty of dancing.",
   },
   {
-    question: "What about gifts?",
+    question: "3/ What about gifts?",
     answer:
       "If it is your wish to bless us with a gift, we would gladly appreciate a contribution to our wishing well.",
   },
 
   {
-    question: "What is the best way to travel to Greece?",
+    question: "4/ What is the best way to travel to Greece?",
     answer:
       "Most international guests will likely fly into Athens International Airport (ATH). From there, you can reach Island Art & Taste by car, taxi, or public transportation. We recommend booking flights early, especially during the peak travel season.",
   },
   {
-    question: "Do I need a visa to travel to Greece?",
+    question: "5/ Do I need a visa to travel to Greece?",
     answer:
       "Citizens of the European Union, Australia, the United States, Malaysia, and many other countries do not require a visa for short stays in Greece. However, please check with your local embassy or consulate for the most up-to-date visa requirements.",
   },
   {
-    question: "What's the weather like in Greece in May?",
+    question: "6/ What's the weather like in Greece in May?",
     answer:
       "May in Greece is pretty much perfect - warm and sunny but not scorching, usually around 20-25°C (68-77°F). Evening temperatures can drop a bit, so pack a light wrap or jacket but we suggest you keep on dancing to keep warm.",
   },
   {
-    question: "What is the local currency and will I need cash?",
+    question: "7/ What is the local currency and will I need cash?",
     answer:
       "The local currency is the Euro (€). Credit and debit cards are widely accepted, but it’s a good idea to carry some cash, especially for small purchases or in more remote areas.",
   },
   {
-    question: "How can we contact you?",
+    question: "8/ How can we contact you?",
     answer:
       "You can contact us on Messenger or via email at dimoandkimo@gmail.com :)",
   },
--- src/components/rsvp/rsvpSubmitted.tsx
+++ src/components/rsvp/rsvpSubmitted.tsx
@@ -21,11 +21,6 @@ const RSVPSubmitted: React.FC<RSVPSubmittedProps> = ({
           message={
             RSVP_MESSAGES[guest] ? RSVP_MESSAGES[guest] : RSVP_MESSAGES.others
           }
-          // message={
-          //   guest === "Cheok Seng Lee"
-          //     ? RSVP_MESSAGES[guest]
-          //     : RSVP_MESSAGES.others
-          // }
         />
       ) : (
         <Message message="Now that’s a real shame. A real shame. We’ll be missin’ ya, but I reckon you got your reasons. Don’t worry—there’ll be a drink raised in your honor." />
